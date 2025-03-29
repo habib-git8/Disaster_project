47461abbeef0138f21d4208d26f3cd498ffb6f8d
Habib Ur Rehman <x24170658@student.ncirl.ie>
2025-03-29 01:26:16 +0000

    Update settings.py

--- disasterapp/disasterapp/settings.py
+++ disasterapp/disasterapp/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # IMAGE_UPLOAD_PATH = os.path.join(BASE_DIR, 'static', 'images')
 
 # PayPal Configuration
